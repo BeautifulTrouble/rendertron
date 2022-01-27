a28ed3f19738c77d0018e01c50fde87433669179
Adrian Carpenter <adriatic.c@gmail.com>
2022-01-27 02:58:08 -0800

    Disk cache; prerendertron invalidates cache

--- prerendertron.py
+++ prerendertron.py
@@ -3,10 +3,12 @@
 import requests
 
 RENDERTRON_PORT = 3000
-TOOL_URL = "https://beautifultrouble.org/toolbox/tool"
 
 j = requests.get("https://api.beautifulrising.org/api/v1/modules").json()
 
 for tool in j:
     print(f"Fetching {tool['title']}...")
-    requests.get(f"http://127.0.0.1:{RENDERTRON_PORT}/render/{TOOL_URL}/{tool['slug']}")
+    requests.get(
+        f"http://127.0.0.1:{RENDERTRON_PORT}/render/"
+        f"https://beautifultrouble.org/toolbox/tool/{tool['slug']}?refreshCache=true"
+    )
