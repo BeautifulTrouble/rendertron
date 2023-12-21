b65b31dc1ffabfa1d57e273c07423a7f4e4066f4
Adrian Carpenter <adriatic.c@gmail.com>
2023-12-21 11:54:32 -0800

    Fix for production

--- prerendertron.py
+++ prerendertron.py
@@ -10,8 +10,7 @@ j = requests.get("https://api.beautifultrouble.org/v2/en/toolbox.json").json()
 for slug, tool in j["tools"].items():
     for lang in tool["langs"]:
         print(f"Rendering {tool['title']} ({lang})")
-        # requests.get(
-        print(
+        requests.get(
             f"http://127.0.0.1:{RENDERTRON_PORT}/render/"
             + "https://beautifultrouble.org/toolbox/"
             + ("" if lang == "en" else f"{lang}/")
