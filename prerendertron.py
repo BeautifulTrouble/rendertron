bb5158c33bbb782b33384bfdef2f3d0b694d9c61
Adrian Carpenter <adriatic.c@gmail.com>
2023-12-21 11:53:47 -0800

    Render all languages

--- prerendertron.py
+++ prerendertron.py
@@ -4,11 +4,16 @@ import requests
 
 RENDERTRON_PORT = 3000
 
-j = requests.get("https://api.beautifultrouble.org/api/v1/modules").json()
 
-for tool in j:
-    print(f"Fetching {tool['title']}...")
-    requests.get(
-        f"http://127.0.0.1:{RENDERTRON_PORT}/render/"
-        f"https://beautifultrouble.org/toolbox/tool/{tool['slug']}?refreshCache=true"
-    )
+j = requests.get("https://api.beautifultrouble.org/v2/en/toolbox.json").json()
+
+for slug, tool in j["tools"].items():
+    for lang in tool["langs"]:
+        print(f"Rendering {tool['title']} ({lang})")
+        # requests.get(
+        print(
+            f"http://127.0.0.1:{RENDERTRON_PORT}/render/"
+            + "https://beautifultrouble.org/toolbox/"
+            + ("" if lang == "en" else f"{lang}/")
+            + f"{slug}?refreshCache=true"
+        )
